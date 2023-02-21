b0b578b31c421244841c9f441d88f028ca161474
Anita-aaa <at9941787@gmail.com>
2023-02-21 16:56:01 +0545

    file

---
+++ salary.py
@@ -0,0 +1,17 @@
+# print("enter a salary")
+# salary= int(input())
+# print("enter a year of service")
+# service=int(input())
+
+# if service>5:
+#     print("bonus is",0.5*salary)
+# else:
+#     print("no bonus")
+
+# a=int(input("enter a quantity"))
+# b=a*100
+# if b>1000:
+#     print("the cost for user is",b-(0.10*b))
+# else:
+#     print( "the cost is",b)
+
